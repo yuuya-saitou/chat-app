450fad1ee51d0349d548db56af7587af684cdbf2
yuuya-saitou <ktkstyyshaba@gmail.com>
2020-08-27 08:22:43 +0900

    画面遷移の実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,5 +1,9 @@
 class RoomsController < ApplicationController
 
+  def index
+    
+  end
+
   def new
     @room = Room.new
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'messages/index'
-  root to: "messages#index"
+  # root to: "messages#index"
+  root to: "rooms#index"
   resources :users, only: [:edit, :update]
   resources :rooms, only: [:new, :create]
 end
